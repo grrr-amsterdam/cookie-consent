9c32dac696308b3502cb473b49ad7bd5157c57ff
Koen Schaft <koen@grrr.nl>
2019-12-12 18:48:08 +0100

    Refactor default CookieConsent API

--- src/cookie-consent.mjs
+++ src/cookie-consent.mjs
@@ -6,8 +6,6 @@ import Storage from './storage';
 
 /**
  * @TODO
- *
- * - Provide defaults...
  * - Trigger dialog with trigger (update settings)
  * - GTM trigger (`event`: `cookieConsent`, types...)
  * - Content checker (e.g. `data-cookie-consent="marketing"`)
@@ -20,49 +18,35 @@ const CookieConsent = settings => {
 
   // Check if LocalStorage is supported.
   const storage = new Storage();
-  if (!storage.supported) {
+  if (!storage.isSupported) {
     return console.warn(`LocalStorage is not supported.`);
   }
 
   // Construct config.
   const config = new Config(settings);
-  const events = new EventDispatcher();
-
-  // Initialize dialog and append it to the DOM.
   const dialog = new Dialog(config);
-  dialog.init();
+  const events = new EventDispatcher();
+  const preferences = new Preferences({ storage, prefix: config.get('prefix') });
 
-  // Handle update...
+  // Initialize dialog and bind `submit` event.
+  dialog.init();
   dialog.on('submit', prefs => {
     preferences.update(prefs);
     events.dispatch('update', prefs);
   });
 
-  // @TODO think about this...
-  if (settings.addDialog) {
-    settings.addDialog(dialog.element);
+  // Show dialog if no preferences are found, or fire the `update` event.
+  if (preferences.has()) {
+    events.dispatch('update', preferences.get());
   } else {
-    document.body.insertBefore(dialog.element, document.body.firstElementChild);
-  }
-
-  // Initialize preferences and show dialog if none are found.
-  const preferences = new Preferences({
-    storage,
-    prefix: config.get('prefix'),
-  });
-  if (!preferences.has()) {
     window.setTimeout(() => dialog.show(), 100);
   }
 
-  // Make it globally available.
-  window[config.get('global')] = {
-    hide: dialog.hide,
-    show: dialog.show,
-    getPreferences: preferences.get,
-  };
-
   return {
-    dialog: dialog.element,
+    getDialog: () => dialog.element,
+    hideDialog: () => dialog.hide,
+    showDialog: () => dialog.show,
+    getPreferences: preferences.get,
     on: events.add,
   };
 };
