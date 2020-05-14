003e54e12ed4e59812b8cc6931e2994e05c1291e
Koen Schaft <koen@grrr.nl>
2020-05-14 14:58:41 +0200

    Add acceptAllButton configuration option

--- src/config-defaults.mjs
+++ src/config-defaults.mjs
@@ -1,10 +1,16 @@
 export const DEFAULTS = {
+  type: 'checkbox',
   prefix: 'cookie-consent',
   append: true,
+  appendDelay: 500,
+  acceptAllButton: false,
   labels: {
     title: 'Cookies & Privacy',
     description: '<p>This site makes use of third-party cookies. Read more in our <a href="/privacy-policy">privacy policy</a>.</p>',
-    button: 'Ok',
+    button: {
+      default: 'Save preferences',
+      acceptAll: 'Accept all',
+    },
     aria: {
       button: 'Confirm cookie settings',
       tabList: 'List with cookie types',
