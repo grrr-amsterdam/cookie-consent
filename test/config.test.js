237485221435955395d2f0da885c1e2f7639ca3a
Bornemisza Sándor Gergely <bornemiszasg@gmail.com>
2020-10-22 19:45:09 +0200

    Testing the config 'append' setting
    The test depends on the default config settings

---
+++ test/config.test.js
@@ -0,0 +1,13 @@
+import Config from '../src/config';
+
+describe('Config', () => {
+  test('append-falsy', () => {
+    const config = Config({append: false});
+    expect(config.get('append')).toBeFalsy();
+  });
+
+  test('append-truthy', () => {
+    const config = Config({});
+    expect(config.get('append')).toBeTruthy();
+  });
+});
