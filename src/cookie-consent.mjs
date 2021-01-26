6458f4b24e55ec99168ea01b0c7aa9b4cf8ef0f1
Daniele Scasciafratte <mte90net@gmail.com>
2021-01-26 15:31:29 +0100

    Fix "Child to insert before is not a child of this node"

--- src/cookie-consent.mjs
+++ src/cookie-consent.mjs
@@ -37,7 +37,7 @@ const CookieConsent = settings => {
   // Append dialog to the DOM, if this is not explicitly prevented.
   if (config.get('append') !== false) {
     const appendEl = document.querySelector('main') || document.body.firstElementChild;
-    document.body.insertBefore(dialog.element, appendEl);
+    appendEl.parentNode.insertBefore(dialog.element, appendEl);
   }
 
   // Show the dialog when no preferences are found. If found, fire the `update` event.
